eb83cb0ef125afb969aa270b77d51ec9703845ff
AfroFlower <anneliesepalmer@gmail.com>
2020-02-14 22:35:18 -0500

    creating and putting together my api key and query url

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,25 @@
 // extra points for external javascript file
+
+$(document).ready(function (){
+
+    var myApiKey = "api_key=BxNABNc8tOV3Dw1Tfz4EBoTbMNH9cBkv";
+    var query = "&q=";
+    var limit = "&limit=";
+    var rating = "&rating=";
+
+
+    var queryUrl = "http://api.giphy.com/v1/gifs/search?" + myApiKey + query + limit + rating + "&lang=en";
+
+    console.log(queryUrl)
+
+
+}
+
 // giphy api key link: https://giphy.api-docs.io/1.0/welcome/access-and-api-keys
+    // public beta key dc6zaTOxFJmzC
+    // my key BxNABNc8tOV3Dw1Tfz4EBoTbMNH9cBkv
+
+    //offical examply from giphy javascript, jQuery
+
+    // var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+    // xhr.done(function(data) { console.log("success got data", data); });
--- script.js
+++
@@ -1,2 +0,0 @@
-// extra points for external javascript file
-// giphy api key link: https://giphy.api-docs.io/1.0/welcome/access-and-api-keys

e5286763f59f39f0f7944a2636fa8588eef257ee
AfroFlower <anneliesepalmer@gmail.com>
2020-02-15 20:08:07 -0500

    I finally have my gifs showing up on my html, now I need to figure out how to limit them

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,26 @@
 // extra points for external javascript file
 
 $(document).ready(function (){
+    // these elements are in the global scope, meaning everything can access them
+    var animalCat = "cat";  
+    var animalDog = "dog";  
+    var animalBird = "bird"; 
+    
+    //first, create and api key
+    var myApiKey = "&api_key=BxNABNc8tOV3Dw1Tfz4EBoTbMNH9cBkv"; 
+    // locate the url for the api
+    var queryUrl = "http://api.giphy.com/v1/gifs/random?&tag=" + animalCat + "&rating=&limit=10&lang=en" + myApiKey;
+
+    console.log(queryUrl)
 
     $("#catButton").on("click", function(){
         
-        
-        //first, create and api key
-        var myApiKey = "&api_key=BxNABNc8tOV3Dw1Tfz4EBoTbMNH9cBkv"; 
-        // the var object we will search for our input
-        var animal = "cat";       
-        // locate the url for the api
-        var queryUrl = "http://api.giphy.com/v1/gifs/random?&tag=" + animal + "&rating=&limit=10&lang=en" + myApiKey;
+        // //first, create and api key
+        // var myApiKey = "&api_key=BxNABNc8tOV3Dw1Tfz4EBoTbMNH9cBkv"; 
+        // // locate the url for the api
+        // var queryUrl = "http://api.giphy.com/v1/gifs/random?&tag=" + animalCat + "&rating=&limit=10&lang=en" + myApiKey;
 
-        console.log(queryUrl)
+        // console.log(queryUrl)
         
         //the .then fucntion calls the object (data) being called
         $.ajax({
@@ -34,6 +42,66 @@ $(document).ready(function (){
 
             // Prepending the catImage to the images div
             $("#gifs-here").prepend(catImage);
+
+        });
+    });
+
+    $("#dogButton").on("click", function(){
+        
+        var queryUrl = "http://api.giphy.com/v1/gifs/random?&tag=" + animalDog + "&rating=&limit=10&lang=en" + myApiKey;
+
+        // console.log(queryUrl)
+        
+        //the .then fucntion calls the object (data) being called
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function(response) {
+            // console.log(response.data[2].images.original.url);
+
+            // Saving the image_original_url property
+            var imageUrl = response.data.images.original.url;
+            console.log(imageUrl)
+
+            // Creating and storing an image tag
+            var dogImage = $("<img>");
+
+            // Setting the catImage src attribute to imageUrl
+            dogImage.attr("src", imageUrl);
+            dogImage.attr("alt", "dog image");
+
+            // Prepending the catImage to the images div
+            $("#gifs-here").prepend(dogImage);
+
+        });
+    });
+
+    $("#birdButton").on("click", function(){
+        
+        var queryUrl = "http://api.giphy.com/v1/gifs/random?&tag=" + animalBird + "&rating=&limit=10&lang=en" + myApiKey;
+
+        // console.log(queryUrl)
+        
+        //the .then fucntion calls the object (data) being called
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function(response) {
+            // console.log(response.data[2].images.original.url);
+
+            // Saving the image_original_url property
+            var imageUrl = response.data.images.original.url;
+            console.log(imageUrl)
+
+            // Creating and storing an image tag
+            var birdImage = $("<img>");
+
+            // Setting the catImage src attribute to imageUrl
+            birdImage.attr("src", imageUrl);
+            birdImage.attr("alt", "dog image");
+
+            // Prepending the catImage to the images div
+            $("#gifs-here").prepend(birdImage);
 
         });
     });

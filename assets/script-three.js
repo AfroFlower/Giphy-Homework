7e6a72ebd2b601634a4f7347e863d974a7486ea3
AfroFlower <anneliesepalmer@gmail.com>
2020-02-17 09:21:21 -0500

    the script-three activates the buttons and appends them to the html page. Now I need to get 10 to append for each button click

--- assets/script-three.js
+++ assets/script-three.js
@@ -1,22 +1,57 @@
 $(document).ready(function (){
-    // these elements are in the global scope, meaning everything can access them
 
-    var animal = " "; 
-    
-    //first, create and api key
-    var myApiKey = "&api_key=BxNABNc8tOV3Dw1Tfz4EBoTbMNH9cBkv"; 
-    // locate the url for the api
-    var queryUrl = "http://api.giphy.com/v1/gifs/random?&tag=" + animal + "&rating=&limit=10&lang=en" + myApiKey;
+    $("button").on("click", function(){
 
-    console.log(queryUrl)
+        // In this case, the "this" keyword refers to the button that was clicked
+        var animal = $(this).attr("data-animal");
 
-    $("#catButton").on("click", function(){
+        // Constructing a URL to search Giphy for the name of the person who said the quote
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=BxNABNc8tOV3Dw1Tfz4EBoTbMNH9cBkv&limit=10";
+        
+        console.log(queryURL)
+        console.log(animal)
+
         //the .then fucntion calls the object (data) being called
         $.ajax({
-            url: queryUrl,
+            url: queryURL,
             method: "GET"
-        }).then(function(response) {
-            console.log(response.data);
+        }).then(function(response){
+            // console.log(response.data);
+
+            // Storing an array of results in the results variable
+            var results = response.data;
+            // console.log(results)
+
+                // Looping over every result item
+                for (var i = 0; i < results.length; i++) {
+
+                // Only taking action if the photo has an appropriate rating
+                if (results[i].rating !== "g" && results[i].rating !== "pg-13") {
+                    // Creating a div for the gif
+                    var gifDiv = $("<div>");
+
+                    // Storing the result item's rating
+                    var rating = results[i].rating;
+
+                    // Creating a paragraph tag with the result item's rating
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    // Creating an image tag
+                    var animalImage = $("<img>");
+
+                    // Giving the image tag an src attribute of a proprty pulled off the
+                    // result item
+                    animalImage.attr("src", results[i].images.fixed_height.url);
+
+                    // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                    gifDiv.append(p);
+                    gifDiv.append(animalImage);
+
+                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                    $("#gifs-here").prepend(gifDiv);
+                };
+            };
+
         });
     });
 

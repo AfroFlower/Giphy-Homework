ec5bb97348af0bca515cf88a22ad6d8062c2675e
AfroFlower <anneliesepalmer@gmail.com>
2020-02-17 18:25:39 -0500

    working on pausing and playing gifs, also appending 10 gifs to my page, not 4

--- assets/script-two.js
+++ assets/script-two.js
@@ -25,41 +25,66 @@ $(document).ready(function (){
             // Creating a div for the gif
             var gifDiv = $("<div class='animals'>");
 
-            // // Storing the result item's rating
-            // var rating = results[i].rating;
-
-            // // Creating a paragraph tag with the result item's rating
-            // var p = $("<p>").text("Rating: " + rating);
-
             // Displaying the rating
             gifDiv.append(p);
             
+            // creating a 'For Loop' for the picked item and inputing "i" when needed
             for (var i = 0; i < topics.length; i++){
 
-            // Storing the result item's rating
-            var rating = results[i].rating;
+                // Storing the result item's rating
+                var rating = results[i].rating;
 
-            // Creating a paragraph tag with the result item's rating
-            var p = $("<p>").text("Rating: " + rating);
-            // Creating an image tag
-            var animalImage = $("<img>");
+                // Creating a paragraph tag with the result item's rating
+                var p = $("<p>").text("Rating: " + rating);
+                // Creating an image tag
+                var animalImage = $("<img>");
 
-            // Giving the image tag an src attribute of a proprty pulled off the
-            // result item
-            animalImage.attr("src", results[i].images.original.url);
-            // console.log(animalImage)
+                // Giving the image tag an src attribute of a proprty pulled off the
+                // result item
+                animalImage.attr("src", results[i].images.original.url);
+                // console.log(animalImage)
 
-            // Appending the paragraph and personImage we created to the "gifDiv" div we created
-            gifDiv.append(p);
-            gifDiv.append(animalImage);
+                // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                gifDiv.append(p);
+                gifDiv.append(animalImage);
 
-            // Putting the entire movie above the previous movies
-            $("#gifs-view").prepend(gifDiv);
+                // Putting the gifabove the previous gifs
+                $("#gifs-view").prepend(gifDiv);
 
-            };
+                // SECTION FOR PAUSING AND PLAYING GIFS // 
+                // var imgSrc = results[i].images.fixed_height_still.url
+                // console.log(imgSrc)
             
+            };
+
         });
     };
+
+        // SECTION FOR PAUSING AND PLAYING GIFS 
+        for (var i = 0; i < topics.length; i++){
+
+            $("img").on("click", function() {
+
+                var imgSrc = results[i].images.fixed_height_still.url
+                console.log(imgSrc)
+
+                // <img src="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" data-still="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" data-animate="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif" data-state="still" class="gif">
+
+                // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+                var state = $(imgSrc).attr("data-state");
+                // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+                // Then, set the image's data-state to animate
+                // Else set src to the data-still value
+                if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+                } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+                };
+            });
+        };
+        
 
     function renderButtons() {
 
@@ -70,18 +95,17 @@ $(document).ready(function (){
         // Looping through the array of animals
         for (var i = 0; i < topics.length; i++){
 
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class of movie-btn to our button
-          a.addClass("gif-btn");
-          // Adding a data-attribute
-          a.attr("data-animal", topics[i]);
-          // Providing the initial button text
-          a.text(topics[i]);
-          // Adding the button to the buttons-view div
-          $("#buttons-view").append(a);  
-
+            // Then dynamicaly generating buttons for each movie in the array
+            // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+            var a = $("<button>");
+            // Adding a class of movie-btn to our button
+            a.addClass("gif-btn");
+            // Adding a data-attribute
+            a.attr("data-animal", topics[i]);
+            // Providing the initial button text
+            a.text(topics[i]);
+            // Adding the button to the buttons-view div
+            $("#buttons-view").append(a);    
         };
     };
 
